80e0132934afdea9efa9413cdcf9f511e2bfb6da
Hyojinko <62235186+Hyojinko@users.noreply.github.com>
2021-05-20 14:59:07 +0900

    Update MainActivity.java

--- QuickBug/app/src/main/java/com/example/quickbug/MainActivity.java
+++ QuickBug/app/src/main/java/com/example/quickbug/MainActivity.java
@@ -1,31 +1,59 @@
 package com.example.quickbug;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Geocoder;
 import android.media.Image;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.webkit.WebView;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Spinner;
 import android.widget.TextView;
+
+import com.naver.maps.geometry.LatLng;
+import com.naver.maps.map.CameraUpdate;
+import com.naver.maps.map.LocationTrackingMode;
+import com.naver.maps.map.MapView;
+import com.naver.maps.map.NaverMap;
+import com.naver.maps.map.OnMapReadyCallback;
+import com.naver.maps.map.overlay.InfoWindow;
+import com.naver.maps.map.overlay.Marker;
+import com.naver.maps.map.util.FusedLocationSource;
 
 import org.w3c.dom.Text;
 
 import java.lang.reflect.Array;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     String[] items = {"IT 융합대학","글로벌센터","예술대학/공과대학","대학원","바이오나노연구원","비전타워/법대/공대2","산학협력관","가천관","교육대학원","중앙도서관","학생회관","기숙사"};
+    private static final String TAG = "MainActivity";
+    private MapView mapView;
+    private static final int LOCATION_PERMISSION_REQUEST_CODE=1000;
+    private FusedLocationSource locationSource;
+    private static final int PERMISSION_REQUEST_CODE = 100;
+    private NaverMap naverMap;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mapView = findViewById(R.id.map_view);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(this);
+
+        locationSource = new FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE);
 
         //출발지 스피너
         Spinner departure = (Spinner)findViewById(R.id.departure);
@@ -63,15 +91,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        //지도 버튼
-        ImageButton map = (ImageButton)findViewById(R.id.imageButton);
-        map.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
-
         //길찾기 버튼
         Button findPath = (Button)findViewById(R.id.btnCompute);
         findPath.setOnClickListener(new View.OnClickListener() {
@@ -90,5 +109,29 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+    //지도상에 마커 표
+    @Override
+    public void onMapReady(@NonNull NaverMap naverMap) {
+        Log.d( TAG, "onMapReady");
+        this.naverMap=naverMap;
+        // 지도상에 마커 표시
+        Marker marker = new Marker();
+        marker.setPosition(new LatLng(37.5670135, 126.9783740));
+        marker.setMap(naverMap);
+
+        // NaverMap 객체 받아서 NaverMap 객체에 위치 소스 지정
+        naverMap.setLocationSource(locationSource);
+
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (locationSource.onRequestPermissionsResult(
+                requestCode, permissions, grantResults)) {
+            if (!locationSource.isActivated()) {
+            }
+            return;
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 }
