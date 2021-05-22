a38395a8d371bbe3b72f5dd07e7a2ba367af70ef
Hyojinko <62235186+Hyojinko@users.noreply.github.com>
2021-05-22 13:33:56 +0900

    Update MainActivity.java

--- QuickBug/app/src/main/java/com/example/quickbug/MainActivity.java
+++ QuickBug/app/src/main/java/com/example/quickbug/MainActivity.java
@@ -56,6 +56,8 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         mapView = findViewById(R.id.map_view);
         mapView.onCreate(savedInstanceState);
         mapView.getMapAsync(this);
+        CameraPosition cameraPosition = new CameraPosition(new LatLng(37.45087165240168, 127.12888504524742),16,20,0);
+        NaverMapOptions options = new NaverMapOptions().camera(cameraPosition).mapType(NaverMap.MapType.Terrain).enabledLayerGroups(NaverMap.LAYER_GROUP_BUILDING).compassEnabled(true).scaleBarEnabled(true).locationButtonEnabled(true);
 
         StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
                 .detectDiskReads().detectDiskWrites().detectNetwork()
@@ -192,17 +194,125 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     //지도상에 마커 표
     @Override
     public void onMapReady(@NonNull NaverMap naverMap) {
-        Log.d( TAG, "onMapReady");
-        this.naverMap=naverMap;
-        // 지도상에 마커 표시
-        Marker marker = new Marker();
-        marker.setPosition(new LatLng(37.5670135, 126.9783740));
-        marker.setMap(naverMap);
-
-        // NaverMap 객체 받아서 NaverMap 객체에 위치 소스 지정
+        Log.d(TAG, "onMapReady");
+        this.naverMap = naverMap;
+        // 지도상에 현위치 표시
         naverMap.setLocationSource(locationSource);
+        naverMap.setLocationTrackingMode(LocationTrackingMode.Follow);
 
+        //건물마다 마커 표시
+
+        //가천관
+        Marker gcmarker = new Marker();
+        gcmarker.setPosition(new LatLng(37.45078969194221, 127.12887595771895));
+        gcmarker.setMap(naverMap);
+
+        //미래2관
+        Marker m2marker = new Marker();
+        m2marker.setPosition(new LatLng(37.45001906066103, 127.12858993867684));
+        m2marker.setMap(naverMap);
+
+        //미래1관
+        Marker m1marker = new Marker();
+        m1marker.setPosition(new LatLng(37.449795952342, 127.12812990821833));
+        m1marker.setMap(naverMap);
+
+        //창의관
+        Marker cmarker = new Marker();
+        cmarker.setPosition(new LatLng(37.44925222720644, 127.12850670247174));
+        cmarker.setMap(naverMap);
+
+        //비전타워
+        Marker vmarker = new Marker();
+        vmarker.setPosition(new LatLng(37.44975246993713, 127.12722948248846));
+        vmarker.setMap(naverMap);
+
+        //IT대학
+        Marker itmarker = new Marker();
+        itmarker.setPosition(new LatLng(37.451003316420696, 127.12715289011525));
+        itmarker.setMap(naverMap);
+
+        //글로벌 센터
+        Marker glmarker = new Marker();
+        glmarker.setPosition(new LatLng(37.451914, 127.127061));
+        glmarker.setMap(naverMap);
+
+        //웅지관
+        Marker wmarker = new Marker();
+        wmarker.setPosition(new LatLng(37.449417729151314, 127.12954248732703));
+        wmarker.setMap(naverMap);
+
+        //학생회관
+        Marker smarker = new Marker();
+        smarker.setPosition(new LatLng(37.45031021741935, 127.13020001963851));
+        smarker.setMap(naverMap);
+
+        //기술관
+        Marker tmarker = new Marker();
+        tmarker.setPosition(new LatLng(37.45037835673896, 127.13037704542226));
+        tmarker.setMap(naverMap);
+
+        //진리관
+        Marker zmarker = new Marker();
+        zmarker.setPosition(new LatLng(37.451250352699894, 127.12974464730218));
+        zmarker.setMap(naverMap);
+
+        //정의관
+        Marker jmarker = new Marker();
+        jmarker.setPosition(new LatLng(37.45133934844123, 127.1303944329334));
+        jmarker.setMap(naverMap);
+
+        //공학관
+        Marker techmarker = new Marker();
+        techmarker.setPosition(new LatLng(37.45160591647754, 127.1280979944333));
+        techmarker.setMap(naverMap);
+
+        //복지관
+        Marker caremarker = new Marker();
+        caremarker.setPosition(new LatLng(37.4514809759294, 127.12896145275238));
+        caremarker.setMap(naverMap);
+
+        //예음관
+        Marker amarker = new Marker();
+        amarker.setPosition(new LatLng(37.45170655219594, 127.12964482180288));
+        amarker.setMap(naverMap);
+
+        //창조관
+        Marker cremarker = new Marker();
+        cremarker.setPosition(new LatLng(37.45223513440125, 127.12868095331365));
+        cremarker.setMap(naverMap);
+
+        //재경관
+        Marker bmarker = new Marker();
+        bmarker.setPosition(new LatLng(37.45264958577441, 127.12936622012947));
+        bmarker.setMap(naverMap);
+
+        //평생교육원
+        Marker submarker = new Marker();
+        submarker.setPosition(new LatLng(37.452734757250994, 127.12999385704026));
+        submarker.setMap(naverMap);
+
+        //아름관
+        Marker ahmarker = new Marker();
+        ahmarker.setPosition(new LatLng(37.45193718705055, 127.13171724265881));
+        ahmarker.setMap(naverMap);
+
+        //중앙도서관
+        Marker libmarker = new Marker();
+        libmarker.setPosition(new LatLng(37.4522800687859, 127.13299724409359));
+        libmarker.setMap(naverMap);
+
+        //세종관
+        Marker sjmarker = new Marker();
+        sjmarker.setPosition(new LatLng(37.453147844616055, 127.13440816146066));
+        sjmarker.setMap(naverMap);
+
+        //기숙사
+        Marker dormarker = new Marker();
+        dormarker.setPosition(new LatLng(37.45435364837424, 127.13546452545525));
+        dormarker.setMap(naverMap);
     }
+
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         if (locationSource.onRequestPermissionsResult(
